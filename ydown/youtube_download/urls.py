b46d05560378b0d814f247af2e619470208c1b7b
naf <avtomax12345@gmail.com>
2024-03-04 23:53:22 +0200

    download resolution added

--- ydown/youtube_download/urls.py
+++ ydown/youtube_download/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.ytb_down, name='ytb_down'),
-    path('download/', views.yt_download)
+    path('download/', views.yt_download),
+    path('download_chosen_resolution/',
+         views.download_chosen_resolution,
+         name='download_chosen_resolution')
 ]
--- ydown/youtube_download/views.py
+++ ydown/youtube_download/views.py
@@ -1,17 +1,63 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest
 from pytube import YouTube
+
+from django.http import FileResponse
+import tempfile
+import os
 
 
 def ytb_down(request):
     return render(request, 'ytb_main.html')
 
 
-def yt_download(request):
-    print('yt_download view is called')  # перевірка виклику
-    url = request.GET.get('url')
-    print(request.GET)
+def download_chosen_resolution(request):
+    if request.method == 'POST':
+        url = request.POST.get('url')
+        resolution = request.POST.get('resolution')
 
-    youtube_obj = YouTube(url)
+        print("download_chosen_resolution: URL:", url)  # Debugging statement
+
+        if url is None or resolution is None:
+            return HttpResponseBadRequest("URL or resolution not provided.")
+
+
+
+
+        try:
+            youtube_obj = YouTube(url)
+        except Exception as e:
+            return HttpResponseBadRequest(f"Error: {e}")
+
+        video = youtube_obj.streams.filter(res=resolution).first()
+        if video:
+            try:
+                temp_file = tempfile.NamedTemporaryFile(delete=False)
+                video.download(output_path=temp_file.name)
+
+                # Close the file to release resources before serving for download
+                temp_file.close()
+
+                with open(temp_file.name, 'rb') as f:
+                    response = FileResponse(f)
+                    response['Content-Disposition'] = f'attachment; filename="{os.path.basename(video.title)}.mp4"'
+
+                    # Delete the temporary file after serving
+                    os.unlink(temp_file.name)
+                    return response
+            except Exception as e:
+                return HttpResponse(f"Error downloading video: {e}")
+        else:
+            return HttpResponse("Video with the chosen resolution not found.")
+
+    return HttpResponse("Failed to download the video.")
+
+
+def yt_download(request):
+    url_from_user = request.GET.get('url_from_user')
+    print('yt_download function ULR_FROM_USER: ', url_from_user)
+
+    youtube_obj = YouTube(url_from_user)
     resolutions = []
     strm_all = youtube_obj.streams.all()
 
@@ -19,14 +65,13 @@ def yt_download(request):
         resolutions.append(i.resolution)
     resolutions = list(dict.fromkeys(resolutions))
 
-    print('resolutions:', resolutions)
-    print('url: ', url)
-
-    embed_link = url.replace("watch?v=", "embed/")
+    embed_link = url_from_user.replace("watch?v=", "embed/")
     embed_link = embed_link.split("&")[0]
 
-
-    print("embed link: ", embed_link)
+    # print("embed link: ", embed_link)
     return render(request,
                   'yt_download.html',
-                  {'resolutions': resolutions, 'embd': embed_link})
+                  {'resolutions': resolutions,  # this is a dictionary with key value pair
+                   'embd': embed_link,
+                   'url_from_user': url_from_user
+                   })
